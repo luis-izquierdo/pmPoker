ec1a2fb0d61304a746d3f58e0926315a3f3a8434
Jorge Diaz <jorge@evolvingweb.ca>
2020-10-19 16:11:30 -0400

    SASS setup & deck readability and design improvement

---
+++ config.rb
@@ -0,0 +1,11 @@
+# We specify compass that we want to use SCSS format
+preferred_syntax = :scss
+
+# Where we will generate the style.css file
+css_dir = 'wwwroot/css'
+
+# Where are our source SASS Files
+sass_dir = 'sass'
+
+# While in Dev, we can use this feature to easily debug the CSS styles
+line_comments = true
